0a217c88702e0193bca35cac703f3ac9876b5682
Tony <nhman2002@gmail.com>
2025-04-14 16:05:01 +0200

    clean up

--- src/ChatServerMain.java
+++ src/ChatServerMain.java
@@ -1,11 +1,5 @@
 import java.rmi.registry.*;
-import java.rmi.server.RMIServerSocketFactory;
-// import java.rmi.server.UnicastRemoteObject;
 import java.io.*;
-// import java.rmi.server.UnicastRemoteObject;
-// import interfaces.ChatServer;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 
 public class ChatServerMain {
     public static void main(String[] args) {
@@ -22,21 +16,7 @@ public class ChatServerMain {
             Integer port = Integer.parseInt(args[0]);
             System.out.println("You are on port: " + port);
 
-
             System.out.println("RMI hostname: " + System.getProperty("java.rmi.server.hostname"));
-
-            // String hostIP = InetAddress.getLocalHost().getHostAddress(); // should print
-            // 34.1.7.20
-            // System.out.println("Chat Server IP:" + hostIP);
-            // System.setProperty("java.rmi.server.hostname", hostIP);
-
-            // Start or connect to RMI registry (use your port or default)
-            // Registry registry = LocateRegistry.createRegistry(port, null, new RMIServerSocketFactory() {
-            //     @Override
-            //     public ServerSocket createServerSocket(int port) throws IOException {
-            //         return new ServerSocket(port, 0, InetAddress.getByName("0.0.0.0"));
-            //     }
-            // });
 
             Registry registry = LocateRegistry.createRegistry(port);
             System.out.println("Registry bound to: " + registry);
@@ -44,8 +24,6 @@ public class ChatServerMain {
 
             ChatServerController server = new ChatServerController(f1);
 
-            // ChatServer stub = (ChatServer) UnicastRemoteObject.exportObject(server, 0);
-            // registry.rebind("ChatService", stub);
             registry.rebind("ChatService", server);
 
             System.out.println("Chat Server ready!");
